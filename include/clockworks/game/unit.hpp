66710574d2d32684dd3ec234ba94e9ca5cd60605
Jonathon Todd <jt314@werfpasta.com>
2014-10-14 04:05:37 -0600

    Alright, so I wrote up the core mechanics for the unit class for the built-in logic engine. The accessors and mutators have been compile-tested, but have not been tested beyond that. Additionally, constructors/initializers have not yet been implemented. Tuesday I won't be ddoing much; I have homework to do and a job interview for which to prepare. 2 of them, actually. Soo yeah. Anyway, yay Unit being (theoretically) operablegit status

---
+++ include/clockworks/game/unit.hpp
@@ -0,0 +1,142 @@
+#ifndef CW_INCLUDE_GAME_UNIT
+#define CW_INCLUDE_GAME_UNIT
+
+#include <utility> //this nets one std::pair and std::get
+#include <vector>
+#include <valarray>
+#include <string>
+#include <cstdlib>
+#include <stdexcept>
+
+namespace ClockWorks {
+  namespace game {
+    class unit {
+    protected:
+
+
+      //This is a array of 2-element arrays.
+      //index[i][0] is the current value
+      //index[i][1] is the maximum value
+      std::valarray<std::pair<int,int>> gauges;
+      std::valarray<int> gauge_modifiers;
+      std::valarray<std::vector<float>> gauge_multipliers;
+
+
+      int level; //Level of Unit
+      std::pair <int,int> xp; //Very special XP Gauge.
+
+
+      std::valarray<int> statistics;
+      std::valarray<int> statistic_modifiers;
+      std::valarray<std::vector<float>> statistic_multipliers;
+
+      //Element-Major
+      std::valarray<std::valarray<float>> elemental_parameters;
+      std::valarray<std::valarray<float>> elemental_parameter_modifiers;
+      std::valarray<std::valarray<std::vector<float>>> elemental_parameter_multipliers;
+
+      //Status-Major
+      std::valarray<std::valarray<float>> status_parameters;
+      std::valarray<std::valarray<float>> status_parameter_modifiers;
+      std::valarray<std::valarray<std::vector<float>>> status_parameter_multipliers;
+
+      void enforce_respect(std::size_t gauge);
+
+    public:	
+
+      const std::string ID; //Unit ID. Assumed to be unique.
+      std::string Name; //The name of the unit
+      std::vector<std::string> Descrip; //Various Descriptors go here.
+
+      //static std::string UNIT_FILE;
+      //static std::string GROWTH_FILE;
+
+      //constructor/destructor
+      //TODO(JT): WRITE UP CONSTRUCTORS/DESTRUCTORS
+
+      unit();
+      //Must specify all 3 - Managing own memory
+      ~unit(); //destructor  so a Unit pointer can resolve properly.
+      unit(const unit& other); //Copy Constructor
+      unit& operator=(const unit& rhs);
+
+
+      // GAUGE FUNCTION DESCRIPTION //
+      // These five functions operate on the gauges, i.e.
+      // the HP/MP/AP/etc, that may be present in-game 
+      // (Not EXP, though, that's handled differently by default)
+
+      // If any of these operate on a gauge that does not exist,
+      // std::out_of_range WILL be thrown.
+
+      // Common arguments across this set:
+      // *std::size_t gauge: the index of the gauge you wish to access.
+      // *bool respect: If respect, then that gauge will be forced
+      // to respect its maximum (i.e. if curr > max, then curr = max)
+
+
+      // GAUGE FUNCTION CALLS //
+
+      // Grabs current/maximum value for the specified gauge. If
+      // modded set, applies modifier, and then all multipliers,
+      // to maximum value before returning.
+      std::pair<int,int> read_gauge(std::size_t gauge, bool modded = true) const;
+
+      // Replaces the gauge at index gauge with a new gauge, as defined
+      // by an std::pair, with <0> as current and <1> as maximum.
+      void set_gauge(std::size_t gauge, const std::pair<int,int>& newgauge, bool respect = true);
+
+      // Increments the current value of the gauge at index gauge by value.
+      void shift_gauge(std::size_t gauge, int value, bool respect = true);
+
+      // Increments the total additive modifier of the maximum
+      // value of the gauge at index gauge by value.
+      void modify_gauge(std::size_t gauge, int value, bool respect = true);
+
+      // Adds value to the multiplicative modifier stack for the
+      // maximum value of the gauge at index gauge by value.
+      void add_gauge_multiplier(std::size_t gauge, float value, bool respect = true);
+
+      // Attempts to remove value from the multiplicative modifier
+      // stack for maximum value of the gauge at index gauge.
+      // Returns true if value is found and removed.
+      // Returns false if value is not found.
+      bool remove_gauge_multiplier(std::size_t gauge, float value, bool respect = true);
+      
+
+      // TODO: Document the rest of this.
+
+      int get_statistic(std::size_t statistic, bool modded = true) const;
+      void set_statistic(std::size_t statistic, int value);
+      void modify_statistic(std::size_t statistic, int value);
+      void add_statistic_multiplier(std::size_t statistic, float value);
+      bool remove_statistic_multiplier(std::size_t statistic, float value);
+
+
+      int get_elemental_parameter(std::size_t element, std::size_t parameter, bool modded = true) const;
+      void set_elemental_parameter(std::size_t element, std::size_t parameter, float value);
+      void modify_elemental_parameter(std::size_t element, std::size_t parameter, int value);
+      void add_elemental_multiplier(std::size_t element, std::size_t parameter, float value);
+      bool remove_elemental_multiplier(std::size_t element, std::size_t parameter, float value);
+
+
+      int get_status_parameter(std::size_t status, std::size_t parameter, bool modded = true) const;
+      void set_status_parameter(std::size_t status, std::size_t parameter, float value);
+      void modify_status_parameter(std::size_t status, std::size_t parameter, int value);
+      void add_status_multiplier(std::size_t status, std::size_t parameter, float value);
+      bool remove_status_multiplier(std::size_t status, std::size_t parameter, float value);
+
+
+
+
+    };
+
+  }
+}
+#endif
+
+
+
+
+
+
---
+++ src/clockworks/game/unit.cpp
@@ -0,0 +1,206 @@
+#include <utility> //for pair and get
+#include <vector>
+#include <string>
+#include <cstdlib>
+
+#include <stdexcept>
+
+#include "../../../include/clockworks/game/unit.hpp"
+
+
+namespace {
+
+  template<class T, typename U>
+  T array_fetch(const std::valarray<T>& arr, std::size_t index, const T& mod, const std::vector<U>& mults, bool modon) {
+    if(index >= arr.size()) throw std::out_of_range("Error: Index out of bounds");
+    T returnme = arr[index];
+    
+    if(modon) {
+      returnme += mod;
+      for (U u: mults) 
+	returnme *= u;
+    }
+    
+    return returnme;
+  }
+
+
+  template<class T>
+  void array_scribe(std::valarray<T>& arr, std::size_t index, const T& newguy) {
+    if(index >= arr.size()) throw std::out_of_range("Error: Index out of bounds");
+    arr[index] = newguy;
+  }
+
+
+  template<typename T>
+  bool vector_remove(std::vector<T> vector, const T& target) {
+    for(std::size_t i = 0; i < vector.size(); i++) {
+      if (vector[i] == target) {
+	vector.erase(vector.begin()+i);
+	return true;
+      }
+    }
+    return false;
+  }
+
+
+}
+
+
+namespace ClockWorks {
+  namespace game {
+
+
+    //protected
+
+
+
+    //public
+
+    void unit::enforce_respect(std::size_t gauge) {
+      int compareme = std::get<1>(gauges[gauge]);
+      compareme += gauge_modifiers[gauge];
+      // Let's be pessimistic and floor everything.
+      for(float f : gauge_multipliers[gauge]) compareme *= f;
+
+      if(std::get<0>(gauges[gauge]) >= compareme) std::get<0>(gauges[gauge]) = compareme;
+
+    }
+    
+    
+    std::pair<int,int> unit::read_gauge(std::size_t gauge, bool modded) const {
+      
+      std::pair<int, int> returnme = gauges[gauge];
+      
+      //If we make it here, then we're in range
+      //so stop checking for std::out_of_range
+      
+      if(modded) {
+	std::get<1>(returnme) += gauge_modifiers[gauge];
+	// Let's be pessimistic and floor everything.
+	for(float f : gauge_multipliers[gauge]) std::get<1>(returnme) *= f;
+      }
+
+      return returnme;
+    }
+
+
+    void unit::set_gauge(std::size_t gauge, const std::pair<int,int>& newgauge, bool respect) {
+      array_scribe(gauges, gauge, newgauge);
+      if(respect) enforce_respect(gauge);
+    }
+
+    void unit::shift_gauge(std::size_t gauge, int value, bool respect) {
+      if(gauge >= gauges.size()) throw std::out_of_range("Error: Gauge index out of bounds");
+      std::get<0>(gauges[gauge]) += value;
+      if(respect) enforce_respect(gauge);
+    }
+
+    void unit::modify_gauge(std::size_t gauge, int value, bool respect) {
+      if(gauge >= gauge_modifiers.size()) throw std::out_of_range("Error: Gauge index is out of bounds");
+      gauge_modifiers[gauge] += value;
+      if(respect) enforce_respect(gauge);
+    }
+
+    void unit::add_gauge_multiplier(std::size_t gauge, float value, bool respect) {
+      if(gauge >= gauges.size()) throw std::out_of_range("Error: Gauge index out of bounds");
+      gauge_multipliers[gauge].push_back(value);
+      if(respect) enforce_respect(gauge);
+    }
+
+    bool unit::remove_gauge_multiplier(std::size_t gauge, float value, bool respect) {
+      if(gauge >= gauges.size()) throw std::out_of_range("Error: Gauge index out of bounds");
+      bool returnme = vector_remove(gauge_multipliers[gauge],value);
+      if(respect) enforce_respect(gauge);
+      return returnme;
+    }
+    
+    
+    
+    int unit::get_statistic(std::size_t statistic, bool modded) const{
+      return array_fetch(statistics,statistic,statistic_modifiers[statistic], statistic_multipliers[statistic], modded);
+    }
+
+    void unit::set_statistic(std::size_t statistic, int value) {
+      array_scribe(statistics,statistic,value);
+    }
+
+    void unit::modify_statistic(std::size_t statistic, int value) {
+      if(statistic >= statistics.size()) throw std::out_of_range("Error: Statistic index out of bounds");
+      statistic_modifiers[statistic] += value;
+    }
+    void unit::add_statistic_multiplier(std::size_t statistic, float value) {
+      if(statistic >= statistics.size()) throw std::out_of_range("Error: Statistic index out of bounds");
+      statistic_multipliers[statistic].push_back(value);
+    }
+    bool unit::remove_statistic_multiplier(std::size_t statistic, float value) {
+      if(statistic >= statistics.size()) throw std::out_of_range("Error: Statistic index out of bounds");
+      return vector_remove(statistic_multipliers[statistic],value);
+    }
+    
+    
+    int unit::get_elemental_parameter(std::size_t element, std::size_t parameter, bool modded) const {
+      if(element >= elemental_parameters.size()) throw std::out_of_range("Error: Element index out of bounds");
+      return array_fetch(elemental_parameters[element],parameter,elemental_parameter_modifiers[element][parameter], elemental_parameter_multipliers[element][parameter], modded);
+    }
+
+    void unit::set_elemental_parameter(std::size_t element, std::size_t parameter, float value) {
+      if(element >= elemental_parameters.size()) throw std::out_of_range("Error: Element index out of bounds");
+      return array_scribe(elemental_parameters[element],parameter,value);
+    }
+
+    void unit::modify_elemental_parameter(std::size_t element, std::size_t parameter, int value) {
+      if(element >= elemental_parameters.size()) throw std::out_of_range("Error: Element index out of bounds");
+      if(parameter >= elemental_parameters[element].size()) throw std::out_of_range("Error: Parameter index out of bounds");
+      elemental_parameter_modifiers[element][parameter] += value;
+    }
+
+    void unit::add_elemental_multiplier(std::size_t element, std::size_t parameter, float value) {
+      if(element >= elemental_parameters.size()) throw std::out_of_range("Error: Element index out of bounds");
+      if(parameter >= elemental_parameters[element].size()) throw std::out_of_range("Error: Parameter index out of bounds");
+      elemental_parameter_multipliers[element][parameter].push_back(value);
+    }
+
+    bool unit::remove_elemental_multiplier(std::size_t element, std::size_t parameter, float value) {
+      if(element >= elemental_parameters.size()) throw std::out_of_range("Error: Element index out of bounds");
+      if(parameter >= elemental_parameters[element].size()) throw std::out_of_range("Error: Parameter index out of bounds");
+      return vector_remove(elemental_parameter_multipliers[element][parameter], value);
+    }
+
+
+
+
+
+    int unit::get_status_parameter(std::size_t status, std::size_t parameter, bool modded) const {
+      if(status >= status_parameters.size()) throw std::out_of_range("Error: Status index out of bounds");
+      return array_fetch(status_parameters[status],parameter,status_parameter_modifiers[status][parameter], status_parameter_multipliers[status][parameter], modded);
+    }
+
+    void unit::set_status_parameter(std::size_t status, std::size_t parameter, float value) {
+      if(status >= status_parameters.size()) throw std::out_of_range("Error: Status index out of bounds");
+      return array_scribe(status_parameters[status],parameter,value);
+    }
+
+    void unit::modify_status_parameter(std::size_t status, std::size_t parameter, int value) {
+      if(status >= status_parameters.size()) throw std::out_of_range("Error: Status index out of bounds");
+      if(parameter >= status_parameters[status].size()) throw std::out_of_range("Error: Parameter index out of bounds");
+      status_parameter_modifiers[status][parameter] += value;
+    }
+
+    void unit::add_status_multiplier(std::size_t status, std::size_t parameter, float value) {
+      if(status >= status_parameters.size()) throw std::out_of_range("Error: Status index out of bounds");
+      if(parameter >= status_parameters[status].size()) throw std::out_of_range("Error: Parameter index out of bounds");
+      status_parameter_multipliers[status][parameter].push_back(value);
+    }
+
+    bool unit::remove_status_multiplier(std::size_t status, std::size_t parameter, float value) {
+      if(status >= status_parameters.size()) throw std::out_of_range("Error: Status index out of bounds");
+      if(parameter >= status_parameters[status].size()) throw std::out_of_range("Error: Parameter index out of bounds");
+      return vector_remove(status_parameter_multipliers[status][parameter], value);
+    }    
+    
+  };
+
+};
+
+
